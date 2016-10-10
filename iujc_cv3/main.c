5d7f550d091b5c5a25195dfb63e73ecddb92fec6
Krupicka David <st49651@upce.cz>
2016-10-10 13:21:24 +0200

    druhy priklad

--- iujc_cv3/main.c
+++ iujc_cv3/main.c
@@ -1,10 +1,22 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define N 5
 
+void priklad1();
+void priklad2();
 
 
 int main(int argc, char *argv[]){
+	//priklad1();
+	priklad2();
+	
+
+	system("pause");
+	return 0;
+}
+
+void priklad1(){
 	int a, b, *pa, *pb;
 
 	pa = &a;
@@ -24,6 +36,58 @@ int main(int argc, char *argv[]){
 	printf("obsah pb: %x\n", pb);
 	printf("adresa prom. pb: %x\n", &pb);
 
-	system("pause");
-	return 0;
+	printf("soucet %d", *pa + *pb);
 }
+
+void priklad2(){
+	int *pmin, *pmax;
+	int pole[N], pom;
+
+	for (int i = 0; i < N; i++){
+		printf("zadej %d. prvek", i);
+		scanf("%d", &pole[i]);
+		//druha varianta zapisu
+		//scanf("%d", pole+i);
+	}
+
+	for (int i = 0; i < N; i++){
+
+		//nastaví ukazatele na první prvek v poli
+		if (i == 0){
+			pmax = &pole[i];
+			pmin = &pole[i];
+		}
+
+		printf("\n%d", pole[i]); //printf("%d", (pole+i));
+		
+		//kontrola maxima
+		if (*pmax < pole[i]){
+			pmax = &pole[i];
+			printf(" - maximum bylo zmeneno");
+		}
+
+		printf(" (max %d) ", *pmax);
+
+		//kontrola minima
+		if (*pmin > pole[i]){
+			pmin = &pole[i];
+			printf(" - minimum bylo zmeneno");
+		}
+
+		printf(" (min %d) ", *pmin);
+
+
+		printf("\n");
+	}
+
+	//prohodit
+	pom = *pmax;
+	*pmax = *pmin;
+	*pmin = pom;
+
+
+	for (int i = 0; i < N; i++){
+		printf("\n[%d]%d\n", i, pole[i]);
+	}
+}
+
